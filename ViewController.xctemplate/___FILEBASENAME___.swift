e98a119e079d384ac3cf5c2fb469c8e687d41d4e
Antoine Lenoir <contact@alenoir.com>
2017-01-27 09:10:39 +0100

    feat(Init): Add View Controller and View Model templates

---
+++ ViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,40 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+import RxSwift
+import RealmSwift
+
+// MARK: Properties and Lifecycle
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    
+    var viewModel: ViewModel! = ViewModel()
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+}
+
+// MARK: Setup
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    func setup() {
+        
+    }
+}
+
+// MARK: UI
+extension ___FILEBASENAMEASIDENTIFIER___ {
+
+}
---
+++ ViewModel.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,35 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+import RxRealm
+import RealmSwift
+import RxSwift
+import Moya
+
+// MARK: Properties and initializers
+class ___FILEBASENAMEASIDENTIFIER___: ViewModel {
+    let disposeBag = DisposeBag()
+
+    init() {
+        self.setupLocalData()
+    }
+}
+
+// MARK: Local Data
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    private func setupLocalData() {
+        let realm = try! Realm()
+        
+    }
+}
+
+// MARK: Networking
+extension ___FILEBASENAMEASIDENTIFIER___ {
+
+}
